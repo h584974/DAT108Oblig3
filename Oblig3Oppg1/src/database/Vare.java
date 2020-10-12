56ca4a080096585734ae7093bd56164a66789e6a
olive <olive@DESKTOP-R044BGP>
2020-10-12 22:03:59 +0200

    oppg1 JPA update

--- Oblig3Oppg1/src/database/Vare.java
+++ Oblig3Oppg1/src/database/Vare.java
@@ -1,5 +1,11 @@
 package database;
 
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 @Entity
 @Table(schema = "dat108oblig3", name = "Vare")
 public class Vare {
@@ -7,14 +13,23 @@ public class Vare {
 	@Id
 	private String vareNavn;
 	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "Liste_Id", referencedColumnName = "Liste_Id")
+	private Handleliste handleliste;
+	
 	public Vare() {}
 	
-	public Vare(String vareNavn) {
+	public Vare(String vareNavn, Handleliste handleliste) {
 		this.vareNavn = vareNavn;
+		this.handleliste = handleliste;
 	}
 	
 	public String getVareNavn() {
 		return vareNavn;
+	}
+	
+	public Handleliste getHandleliste() {
+		return handleliste;
 	}
 
 }
