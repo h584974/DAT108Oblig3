7643eedd8add414aff30cdbaf0b8f034bf2c7e90
olive <olive@LAPTOP-MAKEJ906>
2020-10-14 17:43:13 +0200

    oppg1 slett og leggTil i samme post

--- Oblig3Oppg1/src/handleliste/Handleliste.java
+++ Oblig3Oppg1/src/handleliste/Handleliste.java
@@ -35,16 +35,18 @@ public class Handleliste extends HttpServlet {
 			out.println("</head>");
 			out.println("<body>");
 			out.println("<h1>Handleliste</h1>");
-			out.println("<form method=\"post\" action=\"HandlelisteLeggTil\">");
+			out.println("<form method=\"post\" action=\"HandlelistePost\">");
 			out.println("<input type=\"submit\" value=\"Legg til\">");
 			out.println("<input type=\"text\" name=\"vareNavn\">");
+			out.println("<input type=\"hidden\" name=\"slett\" value=\"false\">");
 			out.println("</form>");
 			
 			// Skriver ut et for melement med hver vare, som sender til HandlelisteSlett ved bruk av Slett knapp.
 			vareliste.forEach(vare -> {
-				out.println("<form method=\"post\" action=\"HandlelisteSlett\">");
+				out.println("<form method=\"post\" action=\"HandlelistePost\">");
 				out.println("<p><input type=\"submit\" value=\"Slett\">&ensp;" + EscapeHTML.escape(vare.getVareNavn()) +"</p>");
 				out.println("<input type=\"hidden\" value=\"" + vare + "\" name=\"vareNavn\">");
+				out.println("<input type=\"hidden\" name=\"slett\" value=\"true\">");
 				out.println("</form>");
 			});
 			
--- Oblig3Oppg1/src/handleliste/HandlelistePost.java
+++ Oblig3Oppg1/src/handleliste/HandlelistePost.java
@@ -1,16 +1,19 @@
 package handleliste;
 
 import java.io.IOException;
+
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import database.Vare;
 import database.VareDAO;
 
-@WebServlet("/HandlelisteSlett")
-public class HandlelisteSlett extends HttpServlet {
+@WebServlet("/HandlelistePost")
+public class HandlelistePost extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -18,17 +21,26 @@ public class HandlelisteSlett extends HttpServlet {
 	private VareDAO vareDAO;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		boolean slett = Boolean.parseBoolean(request.getParameter("slett"));
 		String vareNavn = request.getParameter("vareNavn");
 		
-		// Sjekker om vareNavn != null for sikkerhetskyld, og sletter deretter oppgitt vare fra database.
-		if(vareNavn != null) {
-			vareDAO.slettVare(vareDAO.getVare(vareNavn));
+		if(slett) {
+			// Sjekker om vareNavn != null for sikkerhetskyld, og sletter deretter oppgitt vare fra database.
+			if(vareNavn != null) {
+				vareDAO.slettVare(vareDAO.getVare(vareNavn));
+			}
 		}
-		
+		else {
+			// Sjekker at vare er blitt oppgitt og ikke er tom, og legger så til varen i databasen.
+			if(vareNavn != null && !vareNavn.isBlank()) {
+				Vare nyVare = new Vare(vareNavn);
+				vareDAO.leggTilVare(nyVare);
+			}
+		}
 		response.sendRedirect("Handleliste");
 	}
 
